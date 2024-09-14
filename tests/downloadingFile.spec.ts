83b7f58938f41ba24e5597d58ca673ddc9480864
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2024-09-14 21:45:43 +0200

    Update downloadingFile.spec.ts

--- tests/downloadingFile.spec.ts
+++ tests/downloadingFile.spec.ts
@@ -1,9 +1,8 @@
 import { test } from '@playwright/test';
 
-test('Download a file', async ({ page }) => {
+test('Download a PDF file', async ({ page }) => {
   await page.goto('https://demo.automationtesting.in/FileDownload.html#google_vignette');
   await page.locator('button[aria-label="Consent"]').click();
-  
 
   const download = await Promise.all([
     page.waitForEvent('download'),
@@ -11,16 +10,30 @@ test('Download a file', async ({ page }) => {
   ])
 
   const path = await download[0].path();
-  console.log('Path '+path);
-  // await page.locator('#textbox').click();
-  // await page.locator('#textbox').fill('Welcome to the Playwright workshop');
-  // await page.locator('(//div[@class="panel-heading"])[1]').click();
-  // await page.locator('#createTxt').click();
-  // await page.locator('#link-to-download').click();
-  
+  console.log('Path ' + path);
 
-// await download[0].saveAs(download[0].suggestedFilename());
+  // If wanted to save the file with default fileName
+  // await download[0].saveAs(download[0].suggestedFilename());
 
+  // If wanted to save it with specific Name
   await download[0].saveAs('PlaywrightDownloadedFile.pdf');
+  await page.waitForTimeout(3000);
+});
+
+test('Download a Text file', async ({ page }) => {
+  await page.goto('https://demo.automationtesting.in/FileDownload.html#google_vignette');
+  await page.locator('button[aria-label="Consent"]').click();
+
+  await page.locator('#textbox').click();
+  await page.locator('#textbox').pressSequentially('Welcome to the Playwright workshop');
+  await page.locator('#createTxt').click();
+  await page.locator('#link-to-download').click();
+
+  const download = await Promise.all([
+    page.waitForEvent('download'),
+    page.locator('#link-to-download').click()
+  ])
+
+  await download[0].saveAs(download[0].suggestedFilename());
   await page.waitForTimeout(3000);
 });
