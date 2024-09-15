dd0ed9599916c269cbda2aace28aab91f4b900b1
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2024-09-15 17:26:07 +0200

    Added more base page methods

--- tests/pages/basePage.ts
+++ tests/pages/basePage.ts
@@ -1,23 +1,52 @@
-import {Page, Locator} from '@playwright/test'
+import { Page, Locator } from '@playwright/test'
 
-export default class BasePage{
-    readonly page:Page;
+export default class BasePage {
+    readonly page: Page;
 
-    constructor(page:Page){
+    constructor(page: Page) {
         this.page = page;
     }
 
-    // Common Methods
-
-    async navigateTo(url:string){
+    // Common method to navigate to a URL
+    async navigateTo(url: string) {
         await this.page.goto(url);
     }
 
-    async clickElement(element:Locator){
+    // Common method to click an element
+    async clickElement(element: Locator) {
         await element.click();
     }
 
-    async enterValuesInElement(element:Locator, valuesToEnter:string){
+    // Common method to fill out an element
+    async enterValuesInElement(element: Locator, valuesToEnter: string) {
         await element.fill(valuesToEnter);
+    }
+
+    // Common method to retrieve text from an element
+    async getElementText(element: Locator): Promise<string> {
+        return element.innerText();
+    }
+
+    // Common method to wait for an element to be visible
+    async waitForElementVisible(element: Locator | string) {
+        if (typeof element === 'string') {
+            await this.page.waitForSelector(element, { state: 'visible' });
+        } else {
+            await element.waitFor({ state: 'visible' });
+        }
+    }
+
+    // Common method to wait for an element to be hidden
+    async waitForElementHidden(element: Locator) {
+        if (typeof element === 'string') {
+            await this.page.waitForSelector(element, { state: 'hidden' });
+        } else {
+            await element.waitFor({ state: 'hidden' });
+        }
+    }
+
+    // Common method to take a screenshot
+    async takeScreenshot(fileName: string) {
+        await this.page.screenshot({ path: fileName });
     }
 }
